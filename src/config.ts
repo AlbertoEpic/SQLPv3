9d9488bef111a6e3a4963c58535b84c1fc7557a9
AlbertoEpic <albertolafarga@gmail.com>
2026-07-03 10:06:32 +0200

    config: URL canónica final en Cloudflare Workers

--- src/config.ts
+++ src/config.ts
@@ -183,7 +183,7 @@ export interface SiteConfig {
 export const siteConfig: SiteConfig = {
   // Site Information
   // [CONFIG:SITE_URL]
-  site: "https://albertoepic.github.io/SQLP/",
+  site: "https://sqlp.workers.dev/",
   // [CONFIG:SITE_TITLE]
   title: ".::: SQLP :::.",
   // [CONFIG:HOMEPAGE_TITLE]
